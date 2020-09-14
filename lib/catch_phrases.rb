72645cb0f79ddc2cabf0326b331e530b90007abe
Meredith McIntosh <meredith@mcintosh.io>
2020-09-14 18:55:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
-$status
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario(phrase = "It's-a me, Mario!")
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
   puts phrase
 end
 
